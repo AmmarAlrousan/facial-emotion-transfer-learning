8801cf5c4030bcb62d2200b52af40edc0e70d444
Ammar Alrousan <134939029+AmmarAlrousan@users.noreply.github.com>
2026-01-09 21:12:20 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+# src/config.py
+
+# Update these paths based on your local setup
+DATASET_PATH = "path/to/dataset"
+OUTPUT_DIR = "results"
+MODEL_NAME = "xception_transfer_learning"
+NUM_CLASSES = 6
